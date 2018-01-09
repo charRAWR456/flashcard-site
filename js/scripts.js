f598d4254fa2aa5990d5e842483a16173f31ca08
Jamie Pittak and Manuela Garcia <jamie@>
2018-01-09 13:22:07 -0800

    add slide toggle to all definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,97 @@
 $(function(){
-  $(".clickable").click(function(){
+  $(".javascript").click(function(){
     $(".clickable-javascript").slideToggle();
     $(".javascript-definition").slideToggle();
   })
 
+  $(".operators").click(function(){
+    $(".clickable-operators").slideToggle();
+    $(".operators-definition").slideToggle();
+  })
+
+  $(".variable").click(function(){
+    $(".clickable-variable").slideToggle();
+    $(".variable-definition").slideToggle();
+  })
+
+  $(".naming-conventions").click(function(){
+    $(".clickable-naming-conventions").slideToggle();
+    $(".naming-conventions-definition").slideToggle();
+  })
+
+  $(".functions").click(function(){
+    $(".clickable-functions").slideToggle();
+    $(".functions-definition").slideToggle();
+  })
+
+  $(".method").click(function(){
+    $(".clickable-method").slideToggle();
+    $(".method-definition").slideToggle();
+  })
+
+  $(".arguments").click(function(){
+    $(".clickable-arguments").slideToggle();
+    $(".arguments-definition").slideToggle();
+  })
+
+  $(".parameter").click(function(){
+    $(".clickable-parameter").slideToggle();
+    $(".parameter-definition").slideToggle();
+  })
+
+  $(".return").click(function(){
+    $(".clickable-return").slideToggle();
+    $(".return-definition").slideToggle();
+  })
+
+  $(".chaining-method").click(function(){
+    $(".clickable-chaining-method").slideToggle();
+    $(".chaining-method-definition").slideToggle();
+  })
+
+  $(".string").click(function(){
+    $(".clickable-string").slideToggle();
+    $(".string-definition").slideToggle();
+  })
+
+  $(".boolean").click(function(){
+    $(".clickable-boolean").slideToggle();
+    $(".boolean-definition").slideToggle();
+  })
+
+  $(".undefined").click(function(){
+    $(".clickable-undefined").slideToggle();
+    $(".undefined-definition").slideToggle();
+  })
+
+  $(".nan").click(function(){
+    $(".clickable-nan").slideToggle();
+    $(".nan-definition").slideToggle();
+  })
+
+  $(".escape").click(function(){
+    $(".clickable-escape").slideToggle();
+    $(".escape-definition").slideToggle();
+  })
+
+  $(".alerts").click(function(){
+    $(".clickable-alerts").slideToggle();
+    $(".alerts-definition").slideToggle();
+  })
+
+  $(".comment").click(function(){
+    $(".clickable-comment").slideToggle();
+    $(".comment-definition").slideToggle();
+  })
+
+  $(".jquery").click(function(){
+    $(".clickable-jquery").slideToggle();
+    $(".jquery-definition").slideToggle();
+  })
+
+  $(".attribute").click(function(){
+    $(".clickable-attribute").slideToggle();
+    $(".attribute-definition").slideToggle();
+  })
 
 });
